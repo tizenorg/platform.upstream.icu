edb3b80f5dce9e8846814d7ba7e5a0d86e94ab8e
Myungki Lee <mk5004.lee@samsung.com>
2016-02-02 17:49:17 +0900

    Change timezone path for icu library
    Change-Id: I6a793d07c99cfa493e1ac959ec021c17f5581a5b
    Signed-off-by: Myungki Lee <mk5004.lee@samsung.com>

--- source/common/putil.cpp
+++ source/common/putil.cpp
@@ -666,7 +666,7 @@ extern U_IMPORT char *U_TZNAME[];
 #define TZZONEINFO2     "../usr/share/lib/zoneinfo/"
 #define TZ_ENV_CHECK    "localtime"
 #else
-#define TZDEFAULT       "/etc/localtime"
+#define TZDEFAULT       "/opt/etc/localtime"
 #define TZZONEINFO      "/usr/share/zoneinfo/"
 #endif
 #if U_HAVE_DIRENT_H
@@ -1018,6 +1018,10 @@ uprv_tzname(int n)
 
 #if defined(CHECK_LOCALTIME_LINK) && !defined(DEBUG_SKIP_LOCALTIME_LINK)
     /* Caller must handle threading issues */
+
+    /* for update timezone when real timezone is changed */
+    gTimeZoneBufferPtr = NULL;
+
     if (gTimeZoneBufferPtr == NULL) {
         /*
         This is a trick to look at the name of the link to get the Olson ID
